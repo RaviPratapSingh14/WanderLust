63d5a28f2bbf81f27fc7584547ab005dc8e58315
RaviPratapSingh14 <ravipratapsingh@Ravis-MacBook-Air.local>
2025-07-18 01:06:31 +0530

    Rename Models to temp_models (step 1 for case-sensitive fix)

--- routes/review.js
+++ routes/review.js
@@ -3,11 +3,11 @@ const router = express.Router({mergeParams: true});
 const wrapAsync = require('../utils/wrapAsync.js'); 
 const ExpressError = require('../utils/ExpressError.js'); 
 
-const Review=require("../models/review.js");
-const Listing = require('../models/listing.js');
+const Review=require("../Models/review.js");
+const Listing = require('../Models/listing.js');
 const {validateReview, isLoggedIn, isreviewAuthor}=require("../middleware.js");
 const reviewController=require("../controllers/reviews.js");
-const review=require("../models/review.js");
+const review=require("../Models/review.js");
 
 
 
